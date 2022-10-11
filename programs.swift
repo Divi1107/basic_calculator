22441811295128d90aad038d919748b06b66a8bd
Divi1107 <107640606+Divi1107@users.noreply.github.com>
2022-10-11 11:21:17 +0530

    Update programs.swift

--- programs.swift
+++ programs.swift
@@ -29,3 +29,78 @@ func first(text: String) -> String?
 }
 print("THe first appeared character is:", (first(text:"taylor swift")))
 
+//
+struct Size {
+    var width: Double
+    var height: Double
+    func area() -> Double {
+        
+        print("area is \(width * height)")
+        return width * height
+}
+}
+let someSize = Size( width: 45 , height: 50.5)
+let area = someSize.area()
+
+
+var string = String.init()
+var int = Int.init()
+var bool = Bool.init()
+
+//String()
+struct A{
+    var name : String
+    
+} 
+let some = A(name:"divya")
+print(some.name)
+
+//
+
+struct Person{
+    var name: String
+    func sayHELLO() {
+        print("HELLO! My names \(name).")
+    }
+}
+let person = Person(name:"hitohito") 
+    //instance = structname(pass value)
+person.sayHELLO() //instNCE Created is calling the function
+    //intanceMethod called using . op 
+
+
+
+struct Odometer {
+    var i: Int = 0
+    
+}
+let odometer = Odometer()
+print(odometer.i)
+
+
+    //
+struct Temp{
+    var cel: Double
+    init(cel: Double) {
+        self.cel = cel
+    }
+    init(far: Double) {
+        cel = (far-32)/1.8
+    }
+}
+let  currenttemp = Temp(cel:33)
+let boiling = Temp(far: 212.0)
+print(currenttemp.cel)
+print(boiling.cel)
+  
+//
+
+
+struct temper {
+    var celsius: Double
+    var faren: Double {
+        celsius * 1.8+32
+    }
+}
+let temperat = temper(celsius: 8.0)
+print(temperat.faren)
