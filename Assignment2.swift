e65daca8b228d1c34bfb3c0bce6205e53fe2c4ce
Divi1107 <107640606+Divi1107@users.noreply.github.com>
2022-10-12 10:25:18 +0530

    Update Assignment2.swift

--- Assignment2.swift
+++ Assignment2.swift
@@ -16,3 +16,55 @@ return oneStr.lowercased().sorted() == twoStr.lowercased().sorted()
  print(Home.name)
  Home.anagram(oneStr:"aaabbccdfgg", twoStr:"a3b2c2d1f1g2")
  Home.anagram()
+
+
+///practice prg -> class
+
+//class
+
+class Person {
+    let name: String
+    init(name: String){
+        self.name = name
+    }
+    func sayHello() {
+        print("Helloo")
+    }
+}
+
+let person = Person(name: "Betty")
+print(person.name)
+person.sayHello()
+
+
+class vehicle {
+    var currentspeed = 0.0
+    var des: String {
+        "travelling at \(currentspeed) miles/hr"
+    }
+    func noise(){  }
+}
+let somevehicle = vehicle()
+print("vehicle \(somevehicle.des)")
+
+class cycle : vehicle
+{ var basket = false}
+let bicycle = cycle()
+bicycle.basket = true 
+bicycle.currentspeed = 11.00
+print(bicycle.des)
+
+class tenden: cycle
+{var ppl = 0.0 
+}
+let tendem = tenden()
+tendem.basket = true
+tendem.currentspeed = 22.0
+tendem.ppl = 4
+print("Tanden:\(tenden.des)")
+class train: vehicle {
+    override func noise()
+    {print("choo chooô")}
+}
+let trainn = train()
+train.noise()
